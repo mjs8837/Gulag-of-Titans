3a55e3ae943844ed88c4702695fbd29585adf74e
Michael Santos <mjs8837@rit.edu>
2022-02-08 13:09:14 -0500

    Creating a damage button for the monster and adding a particle effect when enemy health is low. Also imported the text mesh stuff for the button.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,24 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : Monster
 {
+    [SerializeField] Button damageButton;
+    [SerializeField] ParticleSystem lowHealthIndicator;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        damageButton.onClick.AddListener(ButtonAttack);
+        lowHealthIndicator.Pause();
     }
 
     // Update is called once per frame
     void Update()
     {
+        CheckHealth();
+    }
 
+    //Temporary method to cause damage to a monster
+    void ButtonAttack()
+    {
+        Attack(-1.0f, this);
     }
 
     protected override void Attack(float damageDealt, Monster target)
     {
         Debug.Log("This enemy has attacked and dealt " + damageDealt + " damage to " + target.monsterName + "!");
         base.Attack(damageDealt, target);
+    }
+
+    //Method to check how much health an enemy has and act accordingly
+    void CheckHealth()
+    {
+        if (health <= (totalHealth * 0.25f))
+        {
+            lowHealthIndicator.Play();
+        }
+
+        if (health <= 0.0f)
+        {
+            health = 0.0f;
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Monster : MonoBehaviour
 {
     public float health;
+    public float totalHealth;
     public string monsterName;
 
     // Start is called before the first frame update
