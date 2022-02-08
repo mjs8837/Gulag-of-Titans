bb793a11e351cdb4cbbd3ab785ec707254020908
Michael Santos <mjs8837@rit.edu>
2022-02-08 18:07:07 -0500

    Adding a resources folder to hold all assets. This is done for when trying to load prefabs directly from a file. Also added a particle system for when the enemy is below 25% of their total hp

--- Assets/Resources/Scripts/Enemy.cs
+++ Assets/Resources/Scripts/Enemy.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
 
-public class Enemy : Monster
+public class Enemy : Titan
 {
     [SerializeField] Button damageButton;
-    [SerializeField] ParticleSystem lowHealthIndicator;
+    [SerializeField] GameObject lowHealthIndicator;
+
+    GameObject particle;
 
     // Start is called before the first frame update
     void Start()
     {
+        lowHealthIndicator = LoadPrefabFromFile("LowHealthIndicator");
+        particle = Instantiate(lowHealthIndicator, transform.position, Quaternion.identity);
+        particle.transform.parent = transform;
+
         damageButton.onClick.AddListener(ButtonAttack);
-        lowHealthIndicator.Pause();
     }
 
     // Update is called once per frame
@@ -21,13 +27,25 @@ public class Enemy : Monster
         CheckHealth();
     }
 
+    private GameObject LoadPrefabFromFile(string filename)
+    {
+        GameObject loadedObject = (GameObject)Resources.Load("Prefabs/" + filename);
+
+        if (loadedObject == null)
+        {
+            Debug.Log("...no file found - please check the configuration");
+        }
+
+        return loadedObject;
+    }
+
     //Temporary method to cause damage to a monster
     void ButtonAttack()
     {
         Attack(-1.0f, this);
     }
 
-    protected override void Attack(float damageDealt, Monster target)
+    protected override void Attack(float damageDealt, Titan target)
     {
         Debug.Log("This enemy has attacked and dealt " + damageDealt + " damage to " + target.monsterName + "!");
         base.Attack(damageDealt, target);
@@ -38,7 +56,10 @@ public class Enemy : Monster
     {
         if (health <= (totalHealth * 0.25f))
         {
-            lowHealthIndicator.Play();
+            if (particle.GetComponent<ParticleSystem>().isStopped)
+            {
+                particle.GetComponent<ParticleSystem>().Play();
+            }
         }
 
         if (health <= 0.0f)
