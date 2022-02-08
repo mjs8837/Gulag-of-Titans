226d12008327ab99d0d9ce445160cedbaad4c7eb
oracra <nfa8750@rit.edu>
2022-02-08 18:10:35 -0500

    Party basic

--- Assets/Scripts/Party.cs
+++ Assets/Scripts/Party.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Party : MonoBehaviour
 {
+    GameObject[] activeParty;
+    List<GameObject> reserveParty;
     // Start is called before the first frame update
     void Start()
     {
-        
+        activeParty = new GameObject[6];
+        reserveParty = new List<GameObject>();
     }
 
     // Update is called once per frame
@@ -15,4 +18,20 @@ public class Party : MonoBehaviour
     {
         
     }
+
+    public void addTitanActive(GameObject titanToAdd, int position)
+    {
+        if (activeParty[position] != null)
+        {
+           addTitanReserve(activeParty[position]);
+        }
+        activeParty[position] = titanToAdd; 
+    }
+
+    public void addTitanReserve(GameObject titanToAdd)
+    {
+        reserveParty.Add(titanToAdd);
+    }
+
+
 }
